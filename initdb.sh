ff4e0fdc22919595dbef5faf10ea6fc5478541b8
raw34 <raw34@sina.com>
2021-03-28 08:38:01 +0800

    update

--- initdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER IF NOT EXISTS sonarqube;
-    CREATE DATABASE IF NOT EXISTS sonarqube;
-    GRANT ALL PRIVILEGES ON DATABASE sonarqube TO sonarqube;
-EOSQL
